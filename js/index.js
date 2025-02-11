1c972ee43e70bb20e1f8ed93f2cbb8f691fe677b
vikrantpixel6 <vikrant@pixel6.co>
2025-02-11 21:46:05 +0530

    code well formated

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // main js file 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
